ab9cd017e73cdfefc5d6dfc5b9845250027d0b71
Dan Nguyen <nghuudan@gmail.com>
2018-05-01 20:52:28 -0400

    Add exports to index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,8 @@
 export { default as ContextStoreProvider } from './context-store-provider';
+export * from './context-store-provider';
+
 export { default as createContextStore } from './create-context-store';
+export * from './create-context-store';
+
 export { default as withContextStore } from './with-context-store';
+export * from './with-context-store';
